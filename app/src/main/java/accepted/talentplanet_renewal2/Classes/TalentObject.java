1c35db7c88ba0395a1966391b6a499d706eb4d74
kwonhong <mkh9012@kookmin.ac.kr>
2019-05-09 15:33:22 +0900

    Make Layout in Java Code

---
+++ app/src/main/java/accepted/talentplanet_renewal2/Classes/TalentObject.java
@@ -0,0 +1,37 @@
+package accepted.talentplanet_renewal2.Classes;
+
+public class TalentObject {
+    private String title;
+    private int backgroundResourceID;
+    private int iconResourceID;
+
+    public TalentObject(String title, int backgroundResourceID, int iconResourceID) {
+        this.title = title;
+        this.backgroundResourceID = backgroundResourceID;
+        this.iconResourceID = iconResourceID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getBackgroundResourceID() {
+        return backgroundResourceID;
+    }
+
+    public void setBackgroundResourceID(int backgroundResourceID) {
+        this.backgroundResourceID = backgroundResourceID;
+    }
+
+    public int getIconResourceID() {
+        return iconResourceID;
+    }
+
+    public void setIconResourceID(int iconResourceID) {
+        this.iconResourceID = iconResourceID;
+    }
+}
