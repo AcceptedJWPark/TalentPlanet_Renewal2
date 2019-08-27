94ab17e7eeaa70518614037dbda885b3d16b6ca6
kwonhong <mkh9012@kookmin.ac.kr>
2019-08-27 17:28:24 +0900

    메신저 정렬 순서

--- app/src/main/java/accepted/talentplanet_renewal2/Messanger/List/MainActivity.java
+++ app/src/main/java/accepted/talentplanet_renewal2/Messanger/List/MainActivity.java
@@ -108,7 +108,7 @@ public class MainActivity extends AppCompatActivity implements MyFirebaseMessagi
                 "\t\t\t\t\t\t\t\t   FROM   TB_CHAT_LOG D05\n" +
                 "                                   GROUP BY D05.ROOM_ID)) D06 ON D01.ROOM_ID = D06.ROOM_ID" +
                 " WHERE D01.ACTIVATE_FLAG = 'Y'\n" +
-                " AND   D01.MASTER_ID = '" + SaveSharedPreference.getUserId(MainActivity.this) + "'";
+                " AND   D01.MASTER_ID = '" + SaveSharedPreference.getUserId(MainActivity.this) + "' ORDER BY D06.CREATION_DATE ASC";
         Cursor cursor = sqliteDatabase.rawQuery(selectBasicChat, null);
         cursor.moveToFirst();
 
